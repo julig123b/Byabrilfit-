b2a85224b353a3de1726d012b2cc880cf7a9574a
julig123b <byabrilfit@gmail.com>
2025-08-31 19:55:25 -0300

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,24 @@
 import { ReactNode } from 'react';
 import Header from './Header';
 
-type Props = { children: ReactNode };
+type Props = {
+  children: ReactNode;
+};
 
 export default function Layout({ children }: Props) {
   return (
     <>
       <Header />
-      <main style={{ paddingTop: 'var(--header-height)', overflowX: 'hidden' }}>{children}</main>
+      <main style={{ paddingTop: 'var(--header-height)', overflowX: 'hidden' }}>
+        {children}
+      </main>
       <footer
         style={{
-          background: 'var(--color-card)', color: '#ccc',
-          padding: '2rem', textAlign: 'center', marginTop: '4rem'
+          background: 'var(--color-card)',
+          color: '#ccc',
+          padding: '2rem',
+          textAlign: 'center',
+          marginTop: '4rem',
         }}
       >
         <p>&copy; {new Date().getFullYear()} ByAbrilFit. Todos los derechos reservados.</p>
